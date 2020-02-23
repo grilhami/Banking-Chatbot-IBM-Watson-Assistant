31366d50d1d2715a4ce7fcc45b275e9eab6f1dac
Nicholas <nicholasdwiarto@yahoo.com>
2020-02-23 21:24:41 +0700

    Added username and password checker.

--- balance_enquiry.py
+++ balance_enquiry.py
@@ -26,9 +26,8 @@ import sqlite3
         conn.execute("INSERT INTO WATSONCREDENTIALS VALUES ('nicholas', 'nicholas', 10000), ('gilang', 'gilang', 20000);")
         conn.commit()
         print("Records inserted successfully!")
-"""
 
-def get_balance(username, password):
+def get_balance(password):
     conn = sqlite3.connect('database.db')   
     data = {"users": []}
     cursor = conn.execute("SELECT BALANCE, PASS FROM WATSONCREDENTIALS WHERE PASS = ? LIMIT 1", [password])
@@ -46,5 +45,31 @@ def get_balance(username, password):
             "message": "Failed to login!"
         }
 
+
+    conn.close()
+"""
+
+"""
+    True function starts here.
+"""
+
+def helper(dict):
+    conn = sqlite3.connect('database.db')   
+    data = {"users": []}
+    cursor = conn.execute("SELECT * FROM WATSONCREDENTIALS WHERE UNAME = ? LIMIT 1", [dict['username']])
+
+    # Compare Username and Password
+    for row in cursor:
+        if dict['username'] == row[0] and dict['password'] == row[1]:
+            data["users"].append({"username": dict['username'], "password": dict['password'], "balance": row[2]})
+
+            return {
+                "balance": data["users"][0]["balance"],
+                "message": "Successful login!"
+            }
+
+    return {
+        "message": "Wrong username and or password!"
+    }
 
     conn.close()
